27d7d6e1c89e9d4c98b1d5d6f7b7ca7184475247
yuki sasaki <69077828+wiskerpaddy@users.noreply.github.com>
2021-05-01 12:44:22 +0900

    秘文化

--- python_db_R00/hello.py
+++ python_db_R00/hello.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 def getConnection():
     driver='{SQL Server}'
-    server = 'YUUKI-PC\SQLEXPRESS'
-    database = 'test'
+    server = '任意'
+    database = '任意'
     trusted_connection='yes'
     connection = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';DATABASE='+database+';PORT=1433;Trusted_Connection='+trusted_connection+';')
     return connection
@@ -16,7 +16,7 @@ def select_sql():
     connection = getConnection()
     message = "Hello world"
 
-    sql = "SELECT * FROM test_table"
+    sql = "SELECT * FROM 任意"
     cursor = connection.cursor()
     cursor.execute( sql )
     rows = cursor.fetchall()
